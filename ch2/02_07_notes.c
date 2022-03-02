356e305ad6ccf617e83da6d925bc8c12baf18645
Aman Soni <amansoni7410@gmail.com>
2022-03-02 17:46:41 +0530

    chapter 2

---
+++ ch2/02_07_notes.c
@@ -0,0 +1,35 @@
+/* Consider a currency system in which there are notes of seven 
+denominations, namely, Re. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100. If 
+a sum of Rs. N is entered through the keyboard, write a program to 
+compute the smallest number of notes that will combine to give Rs. 
+N*/
+
+#include<stdio.h>
+
+int main()
+
+{
+	int N,p,q,r,s,t,u,sum;
+	
+	printf("Enter the number of notes ");
+	scanf("%d",&N);
+	
+	p=N/100;
+	N=N%100;
+	q=N/50;
+	N=N%50;
+	r=N/10;
+	N=N%10;
+	s=N/5;
+	N=N%5;
+	t=N/2;
+	N=N/2;
+	u=N%1;
+	
+	sum=p+q+r+s+t+u;
+	
+	printf("sum=%d",sum);
+	
+	return 0;
+	
+}
